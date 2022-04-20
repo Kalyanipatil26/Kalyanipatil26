8a4f26e0f7b4d2ffdefdac586c303790bf0dc0a0
Kalyanipatil26 <99474830+Kalyanipatil26@users.noreply.github.com>
2022-04-19 23:22:25 -0700

    Add files via upload

---
+++ encapsulation.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class demo
+{
+private: char sname[10];
+         int s_rollno;
+         int marks;
+        
+
+public: void get();
+        void put();
+};
+void demo::get()
+{
+   cout<<"enter student name"<<endl;
+   cin>>sname;
+   cout<<"enter student roll no"<<endl;
+   cin>>s_rollno;
+   cout<<"enter student mark"<<endl;
+   cin>>marks;
+
+}
+
+void demo::put()
+{
+   cout<<"student name is :"<<sname<<endl;
+   cout<<"student roll no is :"<<s_rollno<<endl;
+   cout<<"student marks is :"<<marks;
+}
+ 
+int main()
+{
+clrscr();
+  demo z;
+  z.get();
+  z.put();
+}
